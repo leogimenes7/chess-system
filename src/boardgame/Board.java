a12546b06124a7a5fcb1447f72aafb5be1744f86
Leonardo Gimenes <lgimenes07@gmail.com>
2019-01-02 15:16:36 -0200

    Chess layer and print the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import javafx.geometry.Pos;
+
 public class Board {
     private int rows;
     private int columns;
@@ -26,5 +28,13 @@ public class Board {
 
     public void setColumns(int columns) {
         this.columns = columns;
+    }
+
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
     }
 }
