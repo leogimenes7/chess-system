60e6f6e350f76478dc24af12061219568436ec8c
Leonardo Gimenes <lgimenes07@gmail.com>
2019-01-08 10:45:15 -0200

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,7 +36,7 @@ public class Program {
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
-                if (captured != null){
+                if (capturedPiece != null){
                     captured.add(capturedPiece);
                 }
 
@@ -46,4 +46,5 @@ public class Program {
             }
         }
     }
+
 }
